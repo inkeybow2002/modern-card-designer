45c81d1773bfd36dcd2d298589a493b3d57057e8
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-30 07:05:28 +0100

    feat: initial commit for src/components/TemplateSelector.js

---
+++ src/components/TemplateSelector.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TemplateSelector() {
+    return (
+        <div className="template-selector">
+            <h2>Select a Template</h2>
+            {/* Template options will go here */}
+        </div>
+    );
+}
+
+export default TemplateSelector;
