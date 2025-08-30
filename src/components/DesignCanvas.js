289001086dabd4f94b267c6fc4ee14b52aca959e
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-30 07:05:27 +0100

    feat: initial commit for src/components/DesignCanvas.js

---
+++ src/components/DesignCanvas.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DesignCanvas() {
+    return (
+        <div className="design-canvas">
+            <h2>Your Design Canvas</h2>
+            {/* Canvas will be rendered here */}
+        </div>
+    );
+}
+
+export default DesignCanvas;
