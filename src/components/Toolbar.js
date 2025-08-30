c1c391f2ce179d6d595a227c7f77494b10ce3c76
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-30 07:05:30 +0100

    feat: initial commit for src/components/Toolbar.js

---
+++ src/components/Toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Toolbar() {
+    return (
+        <div className="toolbar">
+            <button>Add Text</button>
+            <button>Add Image</button>
+            {/* Other design tools go here */}
+        </div>
+    );
+}
+
+export default Toolbar;
