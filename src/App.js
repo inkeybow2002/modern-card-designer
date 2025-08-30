7a5369b344cd9abb2cfcad76a17c8542c1aa594c
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-30 07:05:25 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Modern Card Designer</h1>
+            <p>Create and customize your card designs!</p>
+        </div>
+    );
+}
+
+export default App;
